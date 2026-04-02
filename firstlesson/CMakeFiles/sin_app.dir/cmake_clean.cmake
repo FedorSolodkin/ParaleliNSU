b160f48b8f861afb3541b9b3d1a9a9acb38e232d
FedorSolodkin <artgriin333@gmail.com>
2026-04-02 10:06:36 +0700

    Add files via upload

---
+++ firstlesson/CMakeFiles/sin_app.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sin_app.dir/first.cpp.o"
+  "CMakeFiles/sin_app.dir/first.cpp.o.d"
+  "sin_app"
+  "sin_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sin_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
